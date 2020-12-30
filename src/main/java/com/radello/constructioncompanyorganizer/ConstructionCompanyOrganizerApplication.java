e6bb22207ebcd2439b08479fe830d12007f9f833
Dell Latitude E5530 <pfewfw@wp.pl>
2020-12-30 10:06:23 +0100

    First configuration of the project

---
+++ src/main/java/com/radello/constructioncompanyorganizer/ConstructionCompanyOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.radello.constructioncompanyorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionCompanyOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstructionCompanyOrganizerApplication.class, args);
+    }
+
+}
