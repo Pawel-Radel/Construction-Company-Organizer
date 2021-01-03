99c4d93d4fbd7339dc007e6908470e0b9066dbd4
Dell Latitude E5530 <pfewfw@wp.pl>
2021-01-03 17:40:45 +0100

    Adding newIncome, newCosts, constructionOrders templates.

---
+++ src/main/java/com/radello/constructioncompanyorganizer/controller/controller.java
@@ -0,0 +1,35 @@
+package com.radello.constructioncompanyorganizer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+    @GetMapping
+    public String showIndex(){
+        return "index";
+    }
+
+    @GetMapping ("/listConstructionOrder")
+    public String showConsOrdList(){
+        return "ConstructionOrderList";
+    }
+
+    @GetMapping("/newConstructionOrder")
+    public String shownewConstructionOrderform() {
+        return "ConstructionOrderform";
+    }
+
+    @GetMapping("/newIncome")
+    public String shownewIncomeTemplate(){
+
+        return "newIncome";
+    }
+
+    @GetMapping("/newCost")
+    public String shownewCostTemplate(){
+
+        return "newCost";
+    }
+}
